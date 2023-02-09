efc873fec310544f48334058fde8c27d52121b5e
Artem Valkov <valkov.artyom.fs@gmail.com>
2023-02-09 02:46:56 +0200

    fix

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -9,11 +9,43 @@ export const Projects = ({ data }) => {
   )
 }
 
+/*
 export async function getServerSideProps() {
   const res = await fetch(`https://valkov.xyz/api/projects`)
   const data = await res.json()
   return {
     props: { data }
+  }
+}
+*/
+
+export async function getStaticPaths() {
+  const response = await fetch('https://valkov.xyz/api/projects')
+  const data = await response.json()
+
+  const paths = data.map(project => {
+    return {
+      params: {
+        id: `${project.id.toString()}`
+      }
+    }
+  })
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const { params } = context
+  const response = await fetch(`https://valkov.xyz/api/projects`)
+  const data = await response.json()
+
+  return {
+
+    props: {
+      data
+    }
   }
 }
 
