f5e38a052b6532da80557e043b8d8ef1bf873b0a
Artem Valkov <valkov.artyom.fs@gmail.com>
2023-02-09 02:22:12 +0200

    fix

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,15 +1,15 @@
 import { Projects } from '@/components/projects/projects';
 
 export const ProjectsPage = ({ data }) => {
-  console.log(data)
+
   return (
     <>
-      <Projects data={data}/>
+
     </>
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const res = await fetch(`http://localhost:3000/api/projects`)
   const data = await res.json()
   return {
